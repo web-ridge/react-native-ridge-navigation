d0a02b94a10fb6c150d2c7e79efd2d0b29e5d9a6
Arjen de Vos <arjendevoz@gmail.com>
2022-11-22 15:55:52 +0100

    fix: usePreloadResult backward compatible

--- src/Preloader.ts
+++ src/Preloader.ts
@@ -2,9 +2,9 @@ import type { BaseScreen } from './navigationUtils';
 import { useContext } from 'react';
 import OptimizedContext from './contexts/OptimizedContext';
 
-export function usePreloadResult<T extends BaseScreen>(): ReturnType<
-  T['preload']
-> {
+export function usePreloadResult<T extends BaseScreen>(
+  _: T
+): ReturnType<T['preload']> {
   const preloadResult = useContext(OptimizedContext);
   return preloadResult as any;
 }
