288137f9c27f911855c20d9f47395c5fa3251289
Richard Lindhout <richardlindhout96@gmail.com>
2023-05-18 13:50:24 +0200

    Update lazy.tsx

--- src/lazy.tsx
+++ src/lazy.tsx
@@ -1,32 +1,44 @@
-// https://github.com/ianschmitz/react-lazy-with-preload
-import * as React from 'react';
+//https://github.com/ianschmitz/react-lazy-with-preload
+import { ComponentType, createElement, forwardRef, lazy, useRef } from "react";
 
-export type PreloadableComponent<T extends React.ComponentType<any>> = T & {
-  preload: () => Promise<void>;
+export type PreloadableComponent<T extends ComponentType<any>> = T & {
+    preload: () => Promise<T>;
 };
 
-export default function lazy<T extends React.ComponentType<any>>(
-  factory: () => Promise<{ default: T }>
+export function lazyWithPreload<T extends ComponentType<any>>(
+    factory: () => Promise<{ default: T }>
 ): PreloadableComponent<T> {
-  const LazyComponent = React.lazy(factory);
-  let factoryPromise: Promise<void> | undefined;
-  let LoadedComponent: T | undefined;
+    const ReactLazyComponent = lazy(factory);
+    let PreloadedComponent: T | undefined;
+    let factoryPromise: Promise<T> | undefined;
 
-  const Component = React.forwardRef(function LazyWithPreload(props, ref) {
-    return React.createElement(
-      LoadedComponent ?? LazyComponent,
-      Object.assign(ref ? { ref } : {}, props) as any
-    );
-  }) as any as PreloadableComponent<T>;
+    const Component = forwardRef(function LazyWithPreload(props, ref) {
+        // Once one of these is chosen, we must ensure that it continues to be
+        // used for all subsequent renders, otherwise it can cause the
+        // underlying component to be unmounted and remounted.
+        const ComponentToRender = useRef(
+            PreloadedComponent ?? ReactLazyComponent
+        );
+        return createElement(
+            ComponentToRender.current,
+            Object.assign(ref ? { ref } : {}, props) as any
+        );
+    });
 
-  Component.preload = () => {
-    if (!factoryPromise) {
-      factoryPromise = factory().then((module) => {
-        LoadedComponent = module.default;
-      });
-    }
+    const LazyWithPreload = Component as any as PreloadableComponent<T>;
 
-    return factoryPromise;
-  };
-  return Component;
+    LazyWithPreload.preload = () => {
+        if (!factoryPromise) {
+            factoryPromise = factory().then((module) => {
+                PreloadedComponent = module.default;
+                return PreloadedComponent;
+            });
+        }
+
+        return factoryPromise;
+    };
+
+    return LazyWithPreload;
 }
+
+export default lazyWithPreload;
