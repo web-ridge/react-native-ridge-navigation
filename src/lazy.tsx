c870df6f137f189f3522d67dd48f4f1b6834fc3a
Richard Lindhout <richardlindhout96@gmail.com>
2023-05-22 23:06:34 +0200

    fix deep linking in normal stack

--- src/lazy.tsx
+++ src/lazy.tsx
@@ -1,23 +1,24 @@
-//https://github.com/ianschmitz/react-lazy-with-preload
-import { ComponentType, createElement, forwardRef, lazy, useRef } from 'react';
+import * as React from 'react';
 
-export type PreloadableComponent<T extends ComponentType<any>> = T & {
+export type PreloadableComponent<T extends React.ComponentType<any>> = T & {
   preload: () => Promise<T>;
 };
 
-export function lazyWithPreload<T extends ComponentType<any>>(
+export function lazyWithPreload<T extends React.ComponentType<any>>(
   factory: () => Promise<{ default: T }>
 ): PreloadableComponent<T> {
-  const ReactLazyComponent = lazy(factory);
+  const ReactLazyComponent = React.lazy(factory);
   let PreloadedComponent: T | undefined;
   let factoryPromise: Promise<T> | undefined;
 
-  const Component = forwardRef(function LazyWithPreload(props, ref) {
+  const Component = React.forwardRef(function LazyWithPreload(props, ref) {
     // Once one of these is chosen, we must ensure that it continues to be
     // used for all subsequent renders, otherwise it can cause the
     // underlying component to be unmounted and remounted.
-    const ComponentToRender = useRef(PreloadedComponent ?? ReactLazyComponent);
-    return createElement(
+    const ComponentToRender = React.useRef(
+      PreloadedComponent ?? ReactLazyComponent
+    );
+    return React.createElement(
       ComponentToRender.current,
       Object.assign(ref ? { ref } : {}, props) as any
     );
