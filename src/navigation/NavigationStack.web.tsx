a856f5085dad6655e5c386a3ed377d67e790d6d9
Richard Lindhout <richardlindhout96@gmail.com>
2023-06-21 22:09:48 +0200

    try to fix flash on iOS safari

--- src/navigation/NavigationStack.web.tsx
+++ src/navigation/NavigationStack.web.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { NavigationMotion } from 'navigation-react-mobile';
-import { View, StyleSheet } from 'react-native';
 
 import OptimizedContext, {
   OptimizedContextProvider,
@@ -11,19 +10,27 @@ function NavigationStack({ renderWeb }: { renderWeb?: (key: string) => any }) {
   return (
     <NavigationMotion
       duration={0}
-      renderMotion={(_, scene, __, ___, state, data) => (
-        <View
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              backgroundColor: theme.layout.backgroundColor,
-            },
-          ]}
+      renderMotion={(_, scene, key, ___, state, data) => (
+        <div
+          key={key}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            overflow: 'hidden',
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+            // opacity: 1,
+            backgroundColor: theme.layout.backgroundColor as any,
+          }}
         >
           <OptimizedContextProvider state={state} data={data}>
             {renderWeb?.(state.key) || scene}
           </OptimizedContextProvider>
-        </View>
+        </div>
       )}
     />
   );
